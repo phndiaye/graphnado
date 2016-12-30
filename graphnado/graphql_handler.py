cc1350a3577c8e20257289e326da264013819a38
Philippe Ndiaye <phndiaye@gmail.com>
2016-12-30 21:55:59 +0100

    refuse other methods than get and post

--- graphnado/graphql_handler.py
+++ graphnado/graphql_handler.py
@@ -1,6 +1,15 @@
 import tornado.web
 
 
+GRAPHQL_SUPPORTED_METHODS = ['get', 'post']
+
+
 class GraphQLHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('Hello, World')
+
+    def prepare(self):
+        if self.request.method.lower() not in GRAPHQL_SUPPORTED_METHODS:
+            self.send_error(
+                status_code=405,
+                reason='GraphQL only supports GET and POST requests')
