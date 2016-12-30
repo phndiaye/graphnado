2ce1c83ea8dc7207d46814aea1af585d6711ca72
Philippe Ndiaye <phndiaye@gmail.com>
2016-12-30 21:56:34 +0100

    return errors as json

--- graphnado/graphql_handler.py
+++ graphnado/graphql_handler.py
@@ -1,3 +1,5 @@
+import json
+
 import tornado.web
 
 
@@ -13,3 +15,16 @@ class GraphQLHandler(tornado.web.RequestHandler):
             self.send_error(
                 status_code=405,
                 reason='GraphQL only supports GET and POST requests')
+
+    def write_error(self, status_code, **kwargs):
+        kwargs['status_code'] = status_code
+        if status_code == 405:
+            kwargs['message'] = 'Unsupported Http method'
+        else:
+            kwargs['message'] = kwargs['message'] or 'Unknown Error'
+        self.response = kwargs
+        self._write_json()
+
+    def _write_json(self):
+        output = json.dumps(self.response)
+        self.write(output)
