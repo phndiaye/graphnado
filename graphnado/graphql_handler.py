44f1d686ee6b16c1516116ece8eba9b482d66d40
Philippe Ndiaye <phndiaye@gmail.com>
2017-01-03 00:54:30 +0100

    handle graphql params, preparing them for execution

--- graphnado/graphql_handler.py
+++ graphnado/graphql_handler.py
@@ -1,3 +1,6 @@
+import json
+import re
+
 import tornado.web
 from graphql import GraphQLSchema
 
@@ -21,9 +24,9 @@ class GraphQLHandler(tornado.web.RequestHandler):
                 status_code=405,
                 reason='GraphQL only supports GET and POST requests')
 
-    def write_error(self, status_code, **kwargs):
-        kwargs['status_code'] = status_code
-        if status_code == 405:
+        data = self._parse_body(self.request.body)
+        self.graphql_params = self._get_graphql_params(data)
+
     def get(self):
         query, variables, id, operation_name = self.graphql_params
         if self.enable_graphiql and self._should_render_graphiql():
@@ -34,7 +37,25 @@ class GraphQLHandler(tornado.web.RequestHandler):
                 self.write(output)
             except Exception as e:
                 raise e
-    def _should_render_graphiql(self):
+
+    def post(self):
+        pass
+
+    def _parse_body(self, body):
+        if re.match(r'application/json', self.request.headers['Accept']):
+            return json.loads(body.decode('utf-8'))
+
+        return {}
+
+    def _get_graphql_params(self, request_body):
+        query = self.get_argument('query', None) or request_body.get('query')
+        variables = self.get_argument('variables', None) or \
+            request_body.get('variables')
+        id = self.get_argument('id', None) or request_body.get('id')
+        operation_name = self.get_argument('operationName', None) or \
+            request_body.get('operationName')
+        return [query, variables, id, operation_name]
+
     def _should_render_graphiql(self):
         accept_mimetypes = self.request.headers['Accept'].split(',')
         return True if self._wants_html(accept_mimetypes) else False
